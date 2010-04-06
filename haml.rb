23ae4ffb1ae8ab6b5306356c8d3ff046e09e1846
oriolgual <oriol@codegram.com>
2010-04-06 16:56:13 +0200

    SASS support and filter HAML and SASS files

--- haml.rb
+++ haml.rb
@@ -1,17 +1,31 @@
 module Jekyll
   class Site
     def haml2html
-      haml_path = self.config['haml'] || 'haml'
-      (Dir["*.haml", File.join("_layouts", "*.haml")]).each do |f| 
+      path = self.config['haml_path'] || 'haml'
+      haml_folder = self.config['haml_folder'] || '**/*.haml'
+      compile_haml(path, ["*.haml", haml_folder], /\.haml$/,'.html')
+    end  
+
+    def sass2css
+      path = self.config['sass_path'] || 'sass'
+      sass_folder = self.config['sass_folder'] || '**/*.sass'
+      compile_haml(path, ["*.sass", sass_folder], /\.sass$/,'.css')
+    end  
+
+    private
+
+    def compile_haml(exec_path, files, input_regex, output_extension)
+      Dir.glob(files).each do |f| 
         begin
-          result = `#{haml_path} #{f}`
+          result = `#{exec_path} #{f}`
           raise HamlErrorException.new if result.empty?
-          output_file_name = f.gsub!(/\.haml$/,'.html')
+          puts "Rendering #{f}"
+          output_file_name = f.gsub!(input_regex,output_extension)
           File.open(output_file_name,'w') {|f| f.write(result)} if !File.exists?(output_file_name) or (File.exists?(output_file_name) and result != File.read(output_file_name))
         rescue HamlErrorException => e
         end
       end
-    end  
+    end
   end
 
   class HamlErrorException < Exception
@@ -19,5 +33,13 @@ module Jekyll
 
   AOP.before(Site, :render) do |site_instance, result, args|
     site_instance.haml2html
+  end
+
+  AOP.before(Site, :render) do |site_instance, result, args|
+    site_instance.sass2css
+  end
+
+  AOP.after(Site, :filter_entries) do |site_instance, result, args|
+    result.reject{ |entry| entry.match(/\.haml$/) || entry.match(/\.sass$/) }
   end
 end
